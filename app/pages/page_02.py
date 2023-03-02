3b19994b247f31ea234d5843acbab571dc1cc96d
Laura Meyer <laura-meyer@live.de>
2023-03-02 15:56:22 +0100

    update gif agaaain

--- app/pages/page_02.py
+++ app/pages/page_02.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.header('This is page 2')
 
 if st.button('Click me and see what happens'):
-    st.markdown("![Alt Text](https://giphy.com/embed/393kszFi2PuCEopURN)")
+    st.markdown("![Alt Text](https://giphy.com/gifs/its-friday-393kszFi2PuCEopURN)")

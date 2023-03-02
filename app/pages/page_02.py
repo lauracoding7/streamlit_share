593207084d2b20ebc3bec2b28ec3e30eb68845a8
Laura Meyer <laura-meyer@live.de>
2023-03-02 15:45:30 +0100

    added gif

--- app/pages/page_02.py
+++ app/pages/page_02.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.header('This is page 2')
 
 if st.button('Click me and see what happens'):
-    st.balloons()
+    st.markdown("![Alt Text](https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif)")

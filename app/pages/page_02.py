851d065d15ea45568ab60a765e37e3131a55859f
Laura Meyer <laura-meyer@live.de>
2023-03-02 15:58:00 +0100

    done

--- app/pages/page_02.py
+++ app/pages/page_02.py
@@ -3,4 +3,7 @@ import streamlit as st
 st.header('This is page 2')
 
 if st.button('Click me and see what happens'):
-    st.markdown("![Alt Text](https://giphy.com/gifs/its-friday-393kszFi2PuCEopURN)")
+    st.markdown("![Alt Text](https://giphy.com/embed/393kszFi2PuCEopURN)")
+
+
+#<div style="width:100%;height:0;padding-bottom:83%;position:relative;"><iframe src="https://giphy.com/embed/393kszFi2PuCEopURN" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/its-friday-393kszFi2PuCEopURN">via GIPHY</a></p>
